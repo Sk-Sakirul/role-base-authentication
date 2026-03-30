ce34b9870f3aabc92f1ba3dc056ec43dd3e938cb
Sk-Sakirul <sksakirul484@gmail.com>
2026-03-30 17:25:11 +0530

    add task page

---
+++ my-app/src/app/user/tasks/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function TasksPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>User Tasks Page</h1>
+      <p>This page is under development.</p>
+    </div>
+  );
+}
